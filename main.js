011942388e477ff62ff99d1e034544d40943a9c8
innthomas <innthomas00158@gmail.com>
2022-12-19 09:50:11 +0100

    added button

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const container = document.getElementById("container");
+const button = document.querySelector('button');
 
-const size = 16 * 16
+let size;
 
-for (let i =0; i < size; i ++){
-   const div = document.createElement("div");
-   div.addEventListener("mousemove",function(){
+button.addEventListener('click',function(){
+  size =(prompt("How many squares?",16*16))
+ 
+  container.innerHTML ="";
+  
+  for (let i =0; i < size; i ++){
+     const div = document.createElement("div");
+     div.addEventListener("mousemove",function(){
         this.classList.add('hovered')
-       })
-   container.appendChild(div)
-}
+      })
+      container.appendChild(div)
+   }
+})
